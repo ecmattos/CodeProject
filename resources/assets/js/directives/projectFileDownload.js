b80d184c5655ea1b43a59786bfe689a11b07b128
Eduardo Mattos <ecmattos@ig.com.br>
2016-11-22 23:08:40 -0200

    ProjectFile - Event

--- resources/assets/js/directives/projectFileDownload.js
+++ resources/assets/js/directives/projectFileDownload.js
@@ -1,31 +1,35 @@
 angular.module('app.directives')
 	.directive('projectFileDownload', 
-	['appConfig', 'ProjectFile', function(appConfig, ProjectFile) {
+	['$timeout', 'appConfig', 'ProjectFile', function($timeout, appConfig, ProjectFile) {
 		return {
 			restrict: 'E',
 			templateUrl: appConfig.baseUrl + '/build/views/templates/projectFileDownload.html',
-			link: function(scope, element, attr) {
+			link: function (scope, element, attr) {
+				var anchor = element.children()[0];
+				scope.$on('salvar_arquivo', function(event, data){
+					$(anchor).removeClass('disabled');
+					$(anchor).text('Save File');
+					$(anchor).attr({
+						href: 'data:application-octet-stream;base64,' + data.file,
+						download: data.name
+					});
+
+					$timeout(function(){
+						scope.downloadFile = function() {
+						};
+						$(anchor)[0].click();
+					});
+				});
 			},
-			controller: ['$scope', '$element', '$attrs', '$timeout',
-				function($scope, $element, $attrs, $timeout) {
+			controller: ['$scope', '$element', '$attrs',
+				function($scope, $element, $attrs) {
 					$scope.downloadFile = function() {
-						var anchor = $element.children([0]);
+						var anchor = $element.children()[0];
 						$(anchor).addClass('disabled');
 						$(anchor).text('Wait...');
 
 						ProjectFile.download({id: null, idFile: $attrs.idFile}, function(data) {
-							$(anchor).removeClass('disabled');
-							$(anchor).text('Save File');
-							$(anchor).attr({
-								href: 'data:application-octet-stream;base64,' + data.file,
-								download: data.name
-							});
-
-							$timeout(function(){
-								$scope.downloadFile = function() {
-								};
-								$(anchor)[0].click();
-							});
+							$scope.$emit('salvar_arquivo', data);
 						});
 					};
 				}]
